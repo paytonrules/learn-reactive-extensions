b2a839d2a37bad09fb5fffbb2c1a5e0660800cbc
Eric Smith <paytonrules@gmail.com>
2019-12-15 21:52:29 -0600

    Add react router to link to an exercise

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { SimpleTopAppBar, TopAppBarFixedAdjust } from '@rmwc/top-app-bar';
 import { Drawer, DrawerHeader, DrawerTitle,
          DrawerContent, DrawerSubtitle, DrawerAppContent } from '@rmwc/drawer';
 import { List, ListItem } from '@rmwc/list'
+import { BrowserRouter as Router,
+         Switch, Route, Link } from 'react-router-dom';
 
 const App: React.FC = () => {
     const [response, setResponse] = useState({});
@@ -15,30 +17,41 @@ const App: React.FC = () => {
     }, []);
 
     return (
-        <div className="App">
-            <SimpleTopAppBar title="Reactive Exercises" />
-            <TopAppBarFixedAdjust />
-            <div>
-              <Drawer dismissible open={true}>
-                  <DrawerHeader>
-                      <DrawerTitle>Exercises</DrawerTitle>
-                      <DrawerSubtitle>Learn Some ReactiveX</DrawerSubtitle>
-                  </DrawerHeader>
-                  <DrawerContent>
-                      <List>
-                          <ListItem>Fetch Data</ListItem>
-                          <ListItem>Pluck the Names</ListItem>
-                      </List>
-                  </DrawerContent>
-              </Drawer>
-              <DrawerAppContent>
-                  <div style={{padding: '48px'}}>
-                      <h1>I did it</h1>
-                      <p>{JSON.stringify(response)}</p>
-                  </div>
-              </DrawerAppContent>
-            </div>
-        </div>
+        <Router>
+          <div className="App">
+              <SimpleTopAppBar title="Reactive Exercises" />
+              <TopAppBarFixedAdjust />
+              <div>
+                <Drawer dismissible open={true}>
+                    <DrawerHeader>
+                        <DrawerTitle>Exercises</DrawerTitle>
+                        <DrawerSubtitle>Learn Some ReactiveX</DrawerSubtitle>
+                    </DrawerHeader>
+                    <DrawerContent>
+                        <List>
+                            <Link to="/fetch"><ListItem>Fetch Data</ListItem></Link>
+                            <ListItem>Pluck the Names</ListItem>
+                        </List>
+                    </DrawerContent>
+                </Drawer>
+                <Switch>
+                    <Route path="/fetch">
+                        <DrawerAppContent>
+                          <p>Fetch it money man</p>
+                        </DrawerAppContent>
+                    </Route>
+                    <Route path="/">
+                        <DrawerAppContent>
+                            <div style={{padding: '48px'}}>
+                                <h1>I did it</h1>
+                                <p>{JSON.stringify(response)}</p>
+                            </div>
+                        </DrawerAppContent>
+                    </Route>
+                </Switch>
+              </div>
+          </div>
+        </Router>
     );
 }
 
