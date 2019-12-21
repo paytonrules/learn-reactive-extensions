15d58b8a8f46a0e05521e4388c7b869094da79b2
Eric Smith <paytonrules@gmail.com>
2019-12-20 21:54:59 -0600

    Don't commit the solution :)

--- src/exercises/puzzles.ts
+++ src/exercises/puzzles.ts
@@ -2,5 +2,4 @@ import { Observable } from "rxjs";
 import { Dispatch, SetStateAction } from "react";
 
 export const subscribePuzzle = (observable: Observable<Number>, cb:Dispatch<SetStateAction<number>>) => {
-//    observable.subscribe(x => cb(x.valueOf()));
 }
