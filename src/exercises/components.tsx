e883687ac5b71d6a103c1fe8b6b54f4ff8059f98
Li-Hsuan <lihsuanlung@gmail.com>
2020-07-18 10:29:20 -0400

    Switch from statusText to status for http/2 support
    HTTP/2 does not have the concept of reason phrase, and statusText
    for HTTP/2 is not implemented consistently across browsers. See:
    https://github.com/whatwg/fetch/issues/599

--- src/exercises/components.tsx
+++ src/exercises/components.tsx
@@ -395,7 +395,7 @@ export const ConvertSuccessIntoErrorWithThrow: React.FC = () => {
             directions={directions.convertSuccessfulFetchIntoError}
             headers={[['Error Message']]}
             data={[[errorMessage]]}
-            expectedResult={'Not Found'}
+            expectedResult={404}
             result={errorMessage} />
     )
 }
@@ -471,7 +471,7 @@ export const CreateYourOwnFetch: React.FC = () => {
             directions={directions.createYourOwnFetch}
             headers={[['Results']]}
             data={arrayToRows(messages)}
-            expectedResult={['jojade74', 'Not Found']}
+            expectedResult={['jojade74', 404]}
             result={messages}
         />
     )
